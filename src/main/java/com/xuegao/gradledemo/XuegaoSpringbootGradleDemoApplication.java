71afed85a0de2464d6085969bac73882c284810b
feijiumeng <593436@ky-tech.com.cn>
2021-08-11 18:58:57 +0800

    gradle

---
+++ src/main/java/com/xuegao/gradledemo/XuegaoSpringbootGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.gradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuegaoSpringbootGradleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuegaoSpringbootGradleDemoApplication.class, args);
+    }
+
+}
